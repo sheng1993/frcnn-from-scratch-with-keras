edbbf004c8432523b2d530332611f9ff193f06c6
kentaroy47 <kentaroy@stanford.edu>
2019-07-09 03:47:16 +0000

    fix on VOC parsing

--- keras_frcnn/pascal_voc_parser.py
+++ keras_frcnn/pascal_voc_parser.py
@@ -11,8 +11,8 @@ def get_data(input_path):
 
 	visualise = False
 
-	data_paths = [os.path.join(input_path,s) for s in ['VOC2007', 'VOC2012']]
-	
+	data_paths = [os.path.join(input_path,s) for s in ['VOC2007']] #add VOC2012 if using.
+	print("data path:", data_paths)	
 
 	print('Parsing annotation files')
 
